174b2e030ff446d8dfdb7cdc3bec6647c4c8e617
Man Matěj <matej.man@ibot.cas.cz>
2022-03-24 09:27:36 +0100

    density elevation workon barplot

--- script_envi.R
+++ script_envi.R
@@ -65,118 +65,120 @@ foreach(i=c(1:length(czg.nam))) %dopar% {
 stopCluster(cl)
 
 
-## k-mean clustering white box
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-czg.path<-"q://s_slozky_osobni/Man/clanek_2022_Fpicea_big/"
-czg.nam<-list.files(czg.path,pattern = "*.tif$",full.names = T)
-nam<-list.files(czg.path,pattern = "*.tif$",full.names = F)
-czg.names<-substr(nam,1,nchar(nam)-4)
-czg.dat<-terra::rast(czg.nam)
-sel<-c("DiurnalAnisotropicHeating094",
-       "elevation_10m094",
-       "TopographicPositionIndex50-500m094",
-       "Q005AirTemp_f14t19",                 
-       "Q095AirTemp_f14t19",
-       "MeanPrecipitation_f14t19")
+# selected rasters moved to "d:\Fpicea_rasters\"
+# from 
+# "q://s_slozky_osobni/Man/clanek_2022_Fpicea_big/"
+# and "q://s_slozky_osobni/Man/clanek_2022_Fpicea_big/clusters"
+# k-mean clustering performed in SAGA 7.9.0 pcman
+# try 4 and 5 classes
+# six rasters input to clustering
+#
+# c("PotentialTotalSolarRadiat094",
+#   "elevation_10m094",
+#   "TopographicPositionIndex50-500m094",
+#   "Q005AirTemp_f14t19",                 
+#   "Q095AirTemp_f14t19",
+#   "MeanPrecipitation_f14t19")
+# TopographicPositionIndex50-500m094.tif
+# renamed manually to
+# TopographicPositionIndex50_500m094.tif
 
-inputs=czg.nam[czg.names%in%sel]
-output=paste0(czg.path,"Clusters_6class.tif")
-out_html=paste0(czg.path,"Clusters_6class.html")
-
-install.packages("whitebox", repos="http://R-Forge.R-project.org")
-library(whitebox)
-wbt_init()
-
-wbt_k_means_clustering(inputs=inputs[1:2], output=output, classes=6, out_html,
-                   max_iterations = 10, class_change = 2, initialize = "diagonal",
-                   min_class_size = 10, verbose_mode = FALSE)
-
-
-
-
-
-
-# Pararelize the task
-st.list <- unstack(st)
-names(st.list) <- names(st)
-
-sfInit(parallel=TRUE, cpus=parallel:::detectCores()-1)
-sfLibrary(raster)
-sfLibrary(sf)
-
-e.body <- sfSapply(st.list, fun=extract, y=body_32633)
-sfStop()
-
-body_extract<-cbind(e.body,body_32633)
-
-st_write(body_extract,"body_extract.shp") # writes shapefile with exctracted values
-write.table(body_extract,"body_extract_csv.csv",sep=";",row.names = F,col.names = T) # write CSV with extracted data. 
-
-
-
-
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 
 ## CZG extract
 library(terra)
-r<-rast("d://Fpicea_rasters/czg_complet_ji.tif")
-names(r)
-sel<-c("canopy_height_DMP_DMR",
-       "DiurnalAnisotropicHeating094",
+czg.path<-"d:/Fpicea_rasters/"
+czg.nam<-list.files(czg.path,pattern = "*.tif$",full.names = T)
+nam<-list.files(czg.path,pattern = "*.tif$",full.names = F)
+czg.names<-substr(nam,1,nchar(nam)-4)
+r<-rast(czg.nam)
+names(r)<-czg.names
+
+sel<-c("PotentialTotalSolarRadiat094",
        "elevation_10m094",
-       "TopographicPositionIndex50-500m094",
+       "TopographicPositionIndex50_500m094",
+       "Q005AirTemp_f14t19",                 
+       "Q095AirTemp_f14t19",
+       "MeanPrecipitation_f14t19",
+       "CanopyHeightDMP_DMR094",
+       "Clusters4class",                    
+       "Clusters5class",
        "BiotopType094")
 
+names(r) %in% sel
+
 r<-r[[sel]]
-names(r)
 plot(r[[1]])
-ex<-st_read("extent.gpkg")
-ex.tr<-st_transform(ex,st_crs(czg.dat))
+
+
+
+## random background CZG extract
+ext<-spatSample(r[[sel]],1e5,na.rm=T)
+ext$group<-"background"
+saveRDS(ext,"CZG_random10e5.rds")
+
+## presence CZG extract
+library(sf)
+ex<-st_read("fpicea.gpkg")
+ex.tr<-st_transform(ex,32633)
 v<-vect(ex.tr)
 plot(v,add=T)
 
-## random background CZG extract
-library(terra)
-ext<-spatSample(r[[sel]],1e6,na.rm=T)
-ext$group<-"background"
-saveRDS(ext,"CZG_random10e6.rds")
-
-## presence CZG extract
-data_vec<-vect("fpicea.gpkg")
-ext<-terra::extract(r,data_vec)
+ext<-terra::extract(r,v)
+ext<-ext[,-1]
+fpicea<-cbind(ex,ext)
 ext$group<-"presence"
 saveRDS(ext,"CZG_presence.rds")
 
+st_geometry(fpicea)<-NULL
+
+library(openxlsx)
+write.xlsx(fpicea,"Lokality_F.picea_CMV_mm_cluster.xlsx")
+
 
 ## envicompare
-
-df.rand<-readRDS("CZG_random10e6.rds")
+df.rand<-readRDS("CZG_random10e5.rds")
 df.pres<-readRDS("CZG_presence.rds")
 
 df<-rbind(df.pres,df.rand)
 
 ## elevation
-p<-ggplot(data=df,aes(x=elevation_10m094,linetype=group,colour=group)) + 
-  geom_density(alpha=0.3,aes(x=elevation_10m094),size=1.5,show.legend = F) +
-  scale_linetype_manual(values = c("dashed", "solid"))+
-  scale_color_manual(values = c("black","grey"))+
+library(ggplot2)
+
+p<- ggplot(data = df,alpha=0.3,aes(x=elevation_10m094,fill=group)) + 
+  geom_density() +
+  geom_density(data=df,alpha=0.3,aes(x=elevation_10m094,fill=group)) +
   theme(aspect.ratio = 1) +
-  labs(y="Density") +
-  labs(x="Elevation [m]")
+  labs(y="Density")+
+  labs(x="Elevation")+
+  ggtitle("Distribution ascross altitude")
 print(p)
-
-png("CZG_elevation.png",width = 600,height = 600)
-print(p)
-dev.off()
-names(df)
+ggsave("elevation.png",units="cm",height = 10,width = 10,dpi=300,scale=1.5)
 
 
 
-
-
-
-
-
+# pr<-summary(factor(bex_ext[bex_ext$pa=="presence",c(fcts[i])]))
+# pr<-data.frame(id=names(pr),presence=as.numeric(pr))
+# 
+# bg<-summary(factor(bex_ext[bex_ext$pa=="bg",c(fcts[i])]))
+# bg<-data.frame(id=names(bg),background=as.numeric(bg))
+# 
+# df<-merge(bg,pr)
+# df$presence<-range01(df$presence)
+# df$background<-range01(df$background)
+# 
+# df2<-melt(df)
+# names(df2)<-c(fcts[i],"species","density")
+# p2<-ggplot(df2, aes(x = get(fcts[i]), y= density, fill = species)) +
+#   geom_bar(stat="identity", width=.5, position = "dodge")+
+#   xlab(fcts[i]) +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+# print(p2)
+# 
+# df$Clusters4class
 
 
 
